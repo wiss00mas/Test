c4644a2ee5c787a15a418fc08078c199d64b4087
wissakorn <wissakorn.si@gmail.com>
2021-08-11 19:02:51 +0700

    initial sample project

---
+++ customer-service/src/main/java/com/sample/customer/CustomerServiceApplication.java
@@ -0,0 +1,15 @@
+package com.sample.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+}
